b69c24baff56ce37356083e5739cc5950534e7a1
rupesh-malpani-rfh <rupeshmalpani18@gmail.com>
2020-05-23 22:34:15 +0530

    Update Profile.js

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,11 +1,7 @@
-import React, { useEffect, useContext, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import jwt_decode from 'jwt-decode';
 
-import ProfileContext from '../context/profile/profileContext';
-
 const Profile = () => {
-    const profileContext = useContext(ProfileContext);
-    // const { token } = profileContext;
 
     const [profile, setProfile] = useState({
         fname: '',
@@ -13,10 +9,16 @@ const Profile = () => {
         email: ''
     });
 
+    const token = localStorage.token;
+
     useEffect(() => {
-        // localStorage.setItem('token', token)
+        localStorage.setItem('token', token)
         try {
+
             const token = localStorage.token;
+            if (!token) {
+                return <div>No Token!</div>
+            }
             const decoded = jwt_decode(token);
             console.log('decoded', decoded);
 
@@ -28,16 +30,7 @@ const Profile = () => {
         } catch (err) {
             console.error(err)
         }
-        // const token = localStorage.setItem('token', profileContext.token);
-        // const decoded = jwt_decode(token);
-        // console.log('decoded', decoded)
 
-        // setProfile({
-        //     fname: decoded.user.fname,
-        //     lname: decoded.user.lname,
-        //     email: decoded.user.email
-        // });
-        // loadProfile();
     }, [localStorage.token]);
 
     return (
@@ -56,6 +49,10 @@ const Profile = () => {
                         <td>Email</td>
                         <td>{profile.email}</td>
                     </tr>
+                    <tr>
+                        <td>ID</td>
+                        <td>{profile.email}</td>
+                    </tr>
                 </tbody>
             </table>
         </div>
@@ -63,59 +60,3 @@ const Profile = () => {
 }
 
 export default Profile;
-
-
-// import React, { Component } from 'react'
-// import jwt_decode from 'jwt-decode'
-
-// class Profile extends Component {
-//     constructor() {
-//         super()
-//         this.state = {
-//             fname: '',
-//             lname: '',
-//             email: ''
-//         }
-//     }
-
-//     componentDidMount() {
-//         let token = localStorage.token
-//         const decoded = jwt_decode(token)
-//         console.log('decoded', decoded)
-//         this.setState({
-//             fname: decoded.user.fname,
-//             lname: decoded.user.lname,
-//             email: decoded.user.email
-//         })
-//     }
-
-//     render() {
-//         return (
-//             <div className="container">
-//                 <div className="jumbotron mt-5">
-//                     <div className="col-sm-8 mx-auto">
-//                         <h1 className="text-center">PROFILE</h1>
-//                     </div>
-//                     <table className="table col-md-6 mx-auto">
-//                         <tbody>
-//                             <tr>
-//                                 <td>Fist Name</td>
-//                                 <td>{this.state.fname}</td>
-//                             </tr>
-//                             <tr>
-//                                 <td>Last Name</td>
-//                                 <td>{this.state.lname}</td>
-//                             </tr>
-//                             <tr>
-//                                 <td>Email</td>
-//                                 <td>{this.state.email}</td>
-//                             </tr>
-//                         </tbody>
-//                     </table>
-//                 </div>
-//             </div>
-//         )
-//     }
-// }
-
-// export default Profile
